39bc6c36c26063f3a0d76ee6d7fa10bc4451ff04
Yuya <tsukudani710@gmail.com>
2021-11-18 12:03:34 +0900

    整数一つをコンパイルするコンパイラを作成
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
    
     On branch cc_new
     Your branch is ahead of 'origin/main' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	new file:   Makefile
    	modified:   cc.c
    	new file:   test.sh

---
+++ code/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+assert(){
+	
+    expected="$1"
+    input="$2"
+
+    ./cc "$input" > tmp.s
+	gcc -o tmp tmp.s
+	./tmp
+	actual="$?"
+
+	if [ "$actual" == "$expected" ]
+then
+	    echo "$input =>$actual"
+else
+	    echo "$input => $expected expected, but got $actual"
+	    exit 1
+	fi
+}
+
+
+assert 0 0
+
+assert 42 42
+
+echo ok!
