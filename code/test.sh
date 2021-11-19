42da3db5919353ef3b1e706e7f9147ddb5a462ef
Yuya <tsukudani710@gmail.com>
2021-11-19 16:59:54 +0900

    add parser for basic arithmetic formula(  +,-,*,/ ) expext,expext_num functions are still in construction.

--- code/test.sh
+++ code/test.sh
@@ -11,17 +11,34 @@ assert(){
 
 	if [ "$actual" == "$expected" ]
 then
-	    echo "$input =>$actual"
+	    echo ": $input =>$actual : looks ok. "
 else
 	    echo "$input => $expected expected, but got $actual"
 	    exit 1
 	fi
 }
+#error assert
+assert_e(){
+	
+	input="$1"
+	echo -n "Input : "
+	echo $input
+	./cc "$input" 1>tmp.s 2>error.log
+	cat error.log
+
+}
 
 
+echo -n "Disply integer"
 assert 0 0
-
+echo -n "Displya integer another one"
 assert 42 42
+echo -n "sum and sub test"
 assert 21 "5+20-4"
+echo -n "tokenize test"
 assert 21 "5 + 20 -4"
-echo ok!
+
+
+echo  "Error test"
+assert_e 20++3
+
