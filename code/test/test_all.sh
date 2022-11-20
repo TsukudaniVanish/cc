89713c6a1ad1a84d4176416be8ebad5841b2d29b
Yuya <tsukudani710@gmail.com>
2022-11-20 14:08:56 +0900

    Update add test

---
+++ code/test/test_all.sh
@@ -0,0 +1,25 @@
+#!bin/bash
+
+make test 
+if [ $? -eq 1 ]; then 
+    exit 1
+fi 
+
+make etest 
+if [ $? -eq 1 ]; then 
+    exit 1
+fi
+
+make unit_test
+if [ $? -eq 1 ]; then 
+    exit 1
+fi
+
+for target in `find *.c`
+do 
+    ./cc -f $target
+    if [ $? -eq 1 ]; then 
+        echo $target
+        exit 1
+    fi 
+done
