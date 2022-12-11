b5c27afcf3a904e5e9abb9df95aae5a2ccabefbd
Yuya <tsukudani710@gmail.com>
2022-12-11 18:33:50 +0900

    Update link test

--- code/test/test_link.sh
+++ code/test/test_link.sh
@@ -4,13 +4,14 @@ if [ ! -x ./cc ]; then
     make 
 fi 
 
-./cc -F unittest.c > tmp_ut.s
+./cc -F unittest.c > tmp_unittest.s
 ./cc -F main.c > tmp_main.s
 ./cc -F Map.c > tmp_Map.s
-as -o tmp_ut.o tmp_ut.s
+./cc -F parse.c > tmp_parse.s 
+as -o tmp_unittest.o tmp_unittest.s
 as -o tmp_main.o tmp_main.s  
 as -o tmp_Map.o tmp_Map.s 
-cc -no-pie -g -o tmp Vector.o tokenize.o String.o tmp_Map.o Node.o Token.o tmp_main.o parse.o tmp_ut.o preprocess.o codegenerator.o ./backend/file.o ./backend/error_point.o
+cc -no-pie -g -o tmp Vector.o tokenize.o String.o tmp_Map.s Node.o Token.o tmp_main.o tmp_parse.s tmp_unittest.s preprocess.o codegenerator.o ./backend/file.o ./backend/error_point.o
 
 ./tmp -T
 if [ $? -eq 0 ]; then 
@@ -21,13 +22,13 @@ else
     exit 1
 fi 
 
-# ./tmp -f ./test/test.c > tmp_tmp.s 
-# cc -g -o tmp_tmp tmp_tmp.s 
-# ./tmp_tmp 
-# if [ $? -eq 0 ]; then 
-#     echo "OK!"
-# else 
-#     echo "Failed"
-#     $? = 1
-#     exit 1
-# fi 
+./tmp -f ./test/test.c > tmp_tmp.s 
+cc -g -o tmp_tmp tmp_tmp.s 
+./tmp_tmp 
+if [ $? -eq 0 ]; then 
+    echo "OK!"
+else 
+    echo "Failed"
+    $? = 1
+    exit 1
+fi 
